6968cfc1c4c7e7f7c537406080bafe11fec4efba
Anshika2402 <114644419+Anshika2402@users.noreply.github.com>
2022-12-20 17:19:14 +0530

    Update day 4_2!.py

--- day 4_2!.py
+++ day 4_2!.py
@@ -1,6 +1,15 @@
-def tail_fact(n,a=1):
-    if (n==0):
-        return a
-    return tail_fact(n-1,n*a)
-n=int(input("Enter limit: "))
-print(tail_fact(n))
+def stat():
+    l=[10, 2, 30, 4, 45]
+    n=len(l)
+    s=sum(l)
+    x=s/n
+    print("Mean is: "+str(x))
+    l.sort() 
+    if n%2==0:
+        m1=l[n//2]
+        m2=l[n//2-1]
+        m=(m1+m2)/2
+    else:
+        m=l[n//2]
+    print("Median is: "+str(m))
+stat()
