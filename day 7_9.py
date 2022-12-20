75ec2ab8786d97ebd96c852889f5963501e05776
Anshika2402 <114644419+Anshika2402@users.noreply.github.com>
2022-12-20 16:59:09 +0530

    Update day 7_9.py

--- day 7_9.py
+++ day 7_9.py
@@ -4,13 +4,6 @@ s=input('Enter a string: ')
 n=len(s)
 l=[]
 c=0
-
-#print([s[-i:] for i in range(1,len(s)+1)])
-'''
-for i in range(1,n):
-    print(s[-i:],end=' ')
-'''
- 
 for i in range(1,n):
     l.append(s[-i:])
 print(l)
