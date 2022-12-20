fd1e9311216993dcac7f7618bf71cd80d1efcd0b
Anshika2402 <114644419+Anshika2402@users.noreply.github.com>
2022-12-20 16:59:43 +0530

    Update day 7_8.py

--- day 7_8.py
+++ day 7_8.py
@@ -13,52 +13,6 @@ print('No. of prefixes of string: ',c)
 
 
 
-'''
-beg,end=0,0
-while beg<n:
-    if s[end]==s[end-beg]:
-        print(s[beg:end+1],end=' ')
-        end+=1
-        if end==n:
-            beg+=1
-            end=beg
-    else:
-        beg+=1
-        end=beg
-
-
-print([s[:i] for i in range(1,n)])
- '''
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
