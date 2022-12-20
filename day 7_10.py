e3b5df8e51d1720062282f5a0c87258c2657006c
Anshika2402 <114644419+Anshika2402@users.noreply.github.com>
2022-12-20 16:54:49 +0530

    Update day 7_10.py

--- day 7_10.py
+++ day 7_10.py
@@ -1,19 +1,4 @@
-''' Character by character frequency count 
-
-s=input('Enter a string: ')
-c={}
-for i in s:
-    if i in c:
-        c[i]+=1
-    else:
-        c[i]=1
-print('Frequency of each character in string: ',s,'is --->>')
-print(str(c))
-
-for i in s:
-    x=s.count(i)
-    print('Count of ',i,' is: ',x)
-'''
+''' Character by character frequency count '''
 
 s=input('Enter a string: ')
 r=''
